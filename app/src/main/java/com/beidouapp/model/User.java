fa86277c3ca1e7cbd2ac0d342757413e91113963
carrort <2017401560@qq.com>
2022-02-25 01:34:20 +0000

    update app/src/main/java/com/beidouapp/model/User.java.

--- app/src/main/java/com/beidouapp/model/User.java
+++ app/src/main/java/com/beidouapp/model/User.java
@@ -6,6 +6,11 @@ public class User {
     private String deptId;
     private String userId;
     private boolean isChecked;
+    private String head;
+    private String sex;
+    private String birthday;
+    private String phone;
+    private String email;
 
     public void setNickName(String nickName) {
         this.nickName = nickName;
@@ -45,5 +50,44 @@ public class User {
 
     public void setChecked(boolean checked) {
         isChecked = checked;
+    }
+    public String getHead() {
+        return head;
+    }
+
+    public void setHead(String head) {
+        this.head = head;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
